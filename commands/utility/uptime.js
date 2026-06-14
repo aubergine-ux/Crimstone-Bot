bb17ac45f63e02e3fa4567b9cf7f16555e5b2d5e
aubergine_ux <126033183+aubergine-ux@users.noreply.github.com>
2026-06-13 20:30:50 -0400

    CB v26.12 | /uptime command added

--- commands/utility/uptime.js
+++ commands/utility/uptime.js
@@ -3,16 +3,30 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     // 1. Define the command configuration
     data: new SlashCommandBuilder()
-        .setName('command_name') // Must be lowercase, no spaces
-        .setDescription('A brief description of what this command does'),
+        .setName('uptime') // Must be lowercase, no spaces
+        .setDescription('View Crimstone`s Uptime'),
 
     // 2. Define the execution logic
     async execute(interaction) {
         try {
             // Your command logic goes here!
             
+            let totalSeconds = (interaction.client.uptime / 1000);
+
+            const days = Math.floor(totalSeconds / 86400);
+            totalSeconds %= 86400;
+
+            const hours = Math.floor(totalSeconds / 3600);
+            totalSeconds %= 3600;
+
+            const minutes = Math.floor(totalSeconds / 60);
+            const seconds = Math.floor(totalSeconds % 60);
+
+            const uptimeString = `**Uptime:** ${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+
             // Reply to the user
-            await interaction.reply('Hello! This is a blank template response.');
+            await interaction.reply({ content: uptimeString });
             
         } catch (error) {
             console.error('Error executing command:', error);
