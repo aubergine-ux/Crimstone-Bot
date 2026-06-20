7161fa17f2987b0f9872acc01e13545f008d5415
aubergine_ux <126033183+aubergine-ux@users.noreply.github.com>
2026-06-19 22:13:01 -0400

    CB v26.15 | Updated Index.js

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildMessages,
+	GatewayIntentBits.MessageContent
+	] 
+});
 
 client.commands = new Collection();
 client.cooldowns = new Collection();
