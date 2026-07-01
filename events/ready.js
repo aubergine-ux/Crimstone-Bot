ab7f87622dfeabb6e3b1bbf1a2ba26abdebf5c12
aubergine_ux <126033183+aubergine-ux@users.noreply.github.com>
2026-07-01 09:00:16 -0400

    CB v26.22 | Included Bot Uptime

--- events/ready.js
+++ events/ready.js
@@ -12,6 +12,18 @@ module.exports = {
 				type: ActivityType.Playing,
 			}],
 			status: PresenceUpdateStatus.Online,
-		})
+		});
+
+		setInterval(async () => {
+			const url = "http://192.168.1.157:3001/api/push/c55euiUf9v?status=up&msg=OK&ping=";
+			try {
+					const response = await fetch(url);
+					if (response.ok) {
+							console.log("Sucessfully Pinged Kuma!");
+					}
+			} catch (error) {
+					console.error("Failed to Ping Kuma:", error);
+			}
+		}, 60000);
 	},
 };
