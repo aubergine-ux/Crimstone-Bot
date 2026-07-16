8edda09563be26b41ee6a055f967c79493e85dd3
aubergine_ux <126033183+aubergine-ux@users.noreply.github.com>
2026-07-16 09:16:44 -0400

    CB v26.30 | Edited Presence

--- events/ready.js
+++ events/ready.js
@@ -6,13 +6,15 @@ module.exports = {
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 
-		client.user.setPresence({
-			activities: [{
-				name: 'Protecting Rubia',
-				type: ActivityType.Playing,
-			}],
-			status: PresenceUpdateStatus.Online,
-		});
+		const applyPresence = () => {
+			client.user.setPresence({
+				activities: [{
+					name: 'Protecting Rubia',
+					type: ActivityType.Playing,
+				}],
+				status: PresenceUpdateStatus.Online,
+			});
+		};
 
 		applyPresence();
 		setInterval(applyPresence, 10 * 60 * 1000);
@@ -20,12 +22,12 @@ module.exports = {
 		setInterval(async () => {
 			const url = "http://192.168.1.157:3001/api/push/c55euiUf9v?status=up&msg=OK&ping=";
 			try {
-					const response = await fetch(url);
-					if (response.ok) {
-							console.log("Sucessfully Pinged Kuma!");
-					}
+				const response = await fetch(url);
+				if (response.ok) {
+					console.log("Sucessfully Pinged Kuma!");
+				}
 			} catch (error) {
-					console.error("Failed to Ping Kuma:", error);
+				console.error("Failed to Ping Kuma:", error);
 			}
 		}, 60000);
 	},
