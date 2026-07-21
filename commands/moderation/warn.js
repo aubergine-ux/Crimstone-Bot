c42ba84045a88634016523c234685e9ab03fcaea
aubergine_ux <126033183+aubergine-ux@users.noreply.github.com>
2026-07-20 21:21:43 -0400

    CB v26.35 | Fixed Warn System

--- commands/moderation/warn.js
+++ commands/moderation/warn.js
@@ -1,42 +1,42 @@
-const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
-const { readWarnings } = require('../utility/warnStore.js');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { readWarnings, writeWarnings } = require('../utility/warnStore.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('warnings')
-        .setDescription('View a User\'s warnings')
+        .setName('warn')
+        .setDescription('Sends a Server User a Warning')
         .addUserOption(option =>
-            option.setName('target').setDescription('The User to check').setRequired(true))
+            option.setName('target').setDescription('The User to be Warned.').setRequired(true))
+        .addStringOption(option =>
+            option.setName('reason').setDescription('The Reason for the Warning').setRequired(true))
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
 
     async execute(interaction) {
 
         const user = interaction.options.getUser('target');
+        const reason = interaction.options.getString('reason');
         const guildId = interaction.guild.id;
 
         const warnings = readWarnings();
-        const userWarnings = warnings[guildId]?.[user.id] || [];
 
-        if (userWarnings.length === 0) {
-            await interaction.reply({ content: `**${user.tag}** has no warnings.` });
-            return;
-        }
+        if (!warnings[guildId]) warnings[guildId] = {};
+        if (!warnings[guildId][user.id]) warnings[guildId][user.id] = [];
 
-        const fields = [];
-
-        userWarnings.forEach((warn, index) => {
-            fields.push({
-                name: `Warning #${index + 1}`,
-                value: `**Reason:** ${warn.reason}\n**Moderator:** ${warn.moderator}\n**Date:** <t:${Math.floor(warn.timestamp / 1000)}:R>`,
-            });
+        warnings[guildId][user.id].push({
+            reason: reason,
+            moderator: interaction.user.tag,
+            timestamp: Date.now(),
         });
 
-        const warnEmbed = new EmbedBuilder()
-            .setColor(0xE74C3C)
-            .setTitle(`⚠️ Warnings for ${user.tag}`)
-            .addFields(fields)
-            .setFooter({ text: `Total: ${userWarnings.length}` });
+        writeWarnings(warnings);
 
-        await interaction.reply({ embeds: [warnEmbed] });
+        const warnCount = warnings[guildId][user.id].length;
+
+        try {
+            await user.send(`You have been Warned in **${interaction.guild.name}** for: ${reason}`);
+            await interaction.reply({ content: `User **${user.tag}** has been Warned. They now have ${warnCount} Warning(s).` });
+        } catch (error) {
+            await interaction.reply({ content: `Could not DM **${user.tag}** (DMs turned off), but the Warning has been logged. They now have ${warnCount} Warning(s).` });
+        }
     },
 };
