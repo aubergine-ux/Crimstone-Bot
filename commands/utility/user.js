967727292c333a9e9447fbb5ffd082355d669c35
aubergine_ux <126033183+aubergine-ux@users.noreply.github.com>
2026-08-26 23:07:47 -0400

    CB v26.54 | Updated /user to be more in depth

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -3,8 +3,17 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder().setName('user').setDescription('Provides information about the user.'),
 	async execute(interaction) {
+		const { user, member } = interaction;
+		const createdAt = Math.floor(user.createdTimestamp / 1000);
+		const joinedAt = Math.floor(member.joinedTimestamp / 1000);
+
 		await interaction.reply(
-			`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`,
+			[
+				`This command was run by **${user.username}** (<@${user.id}>).`,
+				`Joined: <t:${joinedAt}:D> (<t:${joinedAt}:R>)`,
+				`Account created: <t:${createdAt}:D> (<t:${createdAt}:R>)`,
+				`Top role: ${member.roles.highest.name} · Roles: ${member.roles.cache.size - 1}`,
+			].join('\n'),
 		);
 	},
 };
